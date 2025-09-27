a5f7c487db765a747d3c19efe1eeb8d93b3b7934
moosaad <muhammedsaad.x1@gmail.com>
2025-09-27 21:40:38 +0300

    finished favourite tab ui

--- lib/features/main_layout/favourite/favourite_tab.dart
+++ lib/features/main_layout/favourite/favourite_tab.dart
@@ -1,12 +1,51 @@
+import 'package:evently_online_sat/core/resources/colors_manager.dart';
+import 'package:evently_online_sat/core/widgets/event_item.dart';
+import 'package:evently_online_sat/models/category_model.dart' show CategoryModel;
+import 'package:evently_online_sat/models/event_model.dart' show EventModel;
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FavouriteTab extends StatelessWidget {
   const FavouriteTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.red,
+    return SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search, color: ColorsManager.blue,),
+                hintText: "Search for Event",
+                hintStyle: GoogleFonts.inter(fontSize: 14.sp, color: ColorsManager.blue, fontWeight: FontWeight.bold
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14.r),
+                  borderSide: BorderSide(color: ColorsManager.blue, width: 1.w)
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14.r),
+                  borderSide: BorderSide(color: ColorsManager.blue, width: 1.w)
+                ),
+              ),
+            ),
+          ),
+          Expanded(child: ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: 20,
+              itemBuilder: (context, index)=> EventItem(event: EventModel(
+            category: CategoryModel.categories[2],
+            title: "Meeting for Updating The Development Method ",
+            description: "Meeting for Updating The Development Method ",
+            dateTime: DateTime.now(),
+            timeOfDay: TimeOfDay.now(),
+          ),)))
+      
+        ],
+      ),
     );
   }
 }
